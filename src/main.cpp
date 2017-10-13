bb02a5d96a28be8db8642ce19064c1634bb28591
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2017-10-13 18:19:48 -0400

    Initial commit with some basic rules in CMakeLists.txt and a basic .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <config.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Starting E-Mud MockServer v" << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << std::endl;
+
+
+    std::cout << "Hello, World" << std::endl;
+    return 0;
+}
